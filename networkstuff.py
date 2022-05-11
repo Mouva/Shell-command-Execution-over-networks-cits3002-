bc9527ecbb696f22220c2267e56660275df25efd
ShiftyWizard <o.lynch@me.com>
2022-05-11 14:38:03 +0800

    um git are you ok

--- networkstuff.py
+++ networkstuff.py
@@ -1,47 +1,93 @@
 # 22973676, Adrian Bedford
 # 22989775, Oliver Lynch
 
-# 4096
 
+# Header 193
+# ----------------
+# Control 1
 # Filename 64
 # Filesize 64
-# Packet Number 32
-# Packet Total 32
+# Byte Offset 64
 
-# Data 3840
+# Data, the rest
 
-# Checksum 64
 
-import socket, subprocess
+import socket, os
 
 socks = []
+HEADER_SIZE = 193
+PACKET_SIZE = 1024
+DATA_SIZE = PACKET_SIZE - HEADER_SIZE
+
 
 class packet:
-    def __init__(self, filename):
-        self.filename = filename.encode('utf-8')
-        self.filesize = 0
-        self.packetNumber = 0
-        self.packetTotal = 0
+    def __init__(self, control, filename, filesize, offset, data):
+        self.control = int(control).to_bytes(1, "little")  # 0
+        self.filename = filename.encode("utf-8")
+        self.filesize = int(filesize).to_bytes(64, "little")
+        self.offset = int(offset).to_bytes(64, "little")
 
-        self.data = b''
-
-        self.check = b''        
+        self.data = bytes(data)
 
     def asBytes(self):
-        return self.filename + bytes(self.filesize) + bytes(self.filesize) + bytes(self.packetNumber) + self.data
+        datasize = [1, 64, 64, 64, DATA_SIZE]
+        data = [self.control, self.filename, self.filesize, self.offset, self.data]
+
+        pack = b""
+
+        for size, param in zip(datasize, data):
+            pack += slackfill(param, size)
+
+        return pack
 
     def send(self, s):
         pass
 
-    def checksum(self):
-        pack = sum(bytearray(self.asBytes()))
-        return pack - (1 << 8) if pack & (1 << (8 - 1)) else pack
+    @staticmethod
+    def unpacket(pack):
+        filesize = int.from_bytes(pack[65:129], "little")
+        offset = int.from_bytes(pack[129:193], "little")
 
-    def validate(self, noBitches):
-        return sum(bytearray(self.asBytes() + bytes(noBitches)))
+        print()
 
 
-def start_server(host='', port=6666):
+        return (
+            pack[0],
+            pack[1:65].decode("utf-8").strip("\x00"),
+            filesize,
+            offset,
+            pack[193:HEADER_SIZE + (filesize - offset)],
+        )
+
+
+# The enpackulator
+def enpacket(filename):
+    packets = []
+
+    with open(filename, "rb") as f:
+        # f.seek(n,0) # Ensure at start of file
+        filesize = os.path.getsize(filename)
+        curpos = 0
+
+        while pack := f.read(DATA_SIZE):
+            packets.append(packet(1, filename, filesize, curpos, pack))
+            curpos = f.tell()
+
+    return packets
+
+
+def stripping(packets):
+    pass
+
+
+def combine(packets):
+    pass
+
+
+def slackfill(data, target):
+        return data + bytes(target - len(data))
+
+def start_server(host="", port=6666):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(10)
@@ -58,7 +104,6 @@ def start_client(hosts, port=6666):
             hostName.append(port)
 
         hostNames.append(tuple(hostName))
-
 
     for host in hostNames:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
